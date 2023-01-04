b0036b1b4732483d92c223f72391b311c2a0cd18
Reuel Caetano <reuelbarbosacaetano@gmail.com>
2023-01-04 16:10:31 -0300

    variables name

---
+++ variables_name.py
@@ -0,0 +1,15 @@
+# val = 3.14
+PI = 3.14
+
+# n = 20
+age = 20
+
+# date = "31/12/2022"
+createdAt = "31/12/2022"
+
+# visible = True
+isVisible = True
+# equal = True
+areEqual = True
+# online = False
+hasOnline = False
