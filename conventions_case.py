336b86233b5ff63f4a96a6a304ae31a095fb5343
Reuel Caetano <reuelbarbosacaetano@gmail.com>
2023-01-04 16:11:11 -0300

    conventions case

---
+++ conventions_case.py
@@ -0,0 +1,16 @@
+
+# camelCase
+fullName = "Joe Doe"
+
+# snake_case
+repeat_password = "*****"
+
+# PascalCase
+Person = "Class"
+
+# Exemplo
+class Person:
+    fullName = "Joe Doe"
+
+    def repeat_password(self):
+        ...
